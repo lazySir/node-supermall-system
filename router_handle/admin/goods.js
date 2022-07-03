a697a5db3ec7197ddf9062256ba09d03a89b4388
lazySir <969060742@qq.com>
2022-07-03 15:59:43 +0800

    完成管理员获取商品分类
    1.完成获取一级分类
    2.完成获取二级分类
    3.完成获取三级分类

--- router_handle/admin/goods.js
+++ router_handle/admin/goods.js
@@ -1,3 +1,60 @@
-exports.getCategory1=(req,res)=>{
-  res.send('ok!')
+const db = require('../../db')
+
+exports.getCategory1 = (req, res) => {
+  //获取商品一级分类
+  const sql = `select * from goodsCategory1`
+  db.query(sql, (err, results) => {
+    if (err) return res.cc(err)
+    if (results == null) return res.cc('faile')
+    results = results.map((item) => {
+      //删除是否已删除的属性
+      delete item.is_delete
+      return item
+    })
+    res.json({
+      code: 200,
+      message: '成功',
+      data: results,
+    })
+  })
+}
+
+exports.getCategory2 = (req, res) => {
+  //获取一级分类的id
+  category1Id = req.params.category1Id
+  //获取二级分类
+  const sql = `select * from goodsCategory2 where parent_id=?`
+  db.query(sql, category1Id, (err, results) => {
+    if (err) return res.cc(err)
+    if (results == null) return res.cc('无记录')
+    results = results.map((item) => {
+      delete item.is_delete
+      return item
+    })
+    res.json({
+      code: 200,
+      message: '成功',
+      data: results,
+    })
+  })
+}
+//商品三级分类
+exports.getCategory3 = (req, res) => {
+    //获取二级分类的id
+    category2Id = req.params.category2Id
+    //获取三级分类
+  const sql = `select * from goodsCategory3 where parent_id=?`
+  db.query(sql, category2Id, (err, results) => {
+    if (err) return res.cc(err)
+    if (results == null) return res.cc('faile')
+    results = results.map((item) => {
+      delete item.is_delete
+      return item
+    })
+    res.json({
+      code: 200,
+      message: '成功',
+      data: results,
+    })
+  })
 }
---
+++ schema/admin/goods.js
@@ -0,0 +1,14 @@
+//校验管理员获取商品接口的规则
+const joi = require('@hapi/joi')
+const category1Id =joi.number().required().error(new Error('一级分类id必须为数值！'))
+const category2Id =joi.number().required().error(new Error('一级分类id必须为数值！'))
+exports.admin_schema_category2Id = {
+  params:{
+    category1Id
+  }
+}
+exports.admin_schema_category3Id = {
+  params:{
+    category1Id
+  }
+}
