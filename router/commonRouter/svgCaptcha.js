3774a331fa8811c13e883ca5fca285c55be1df0f
lazySir <969060742@qq.com>
2023-09-17 14:11:26 +0800

    获取图片验证码接口
    1.要刷新获取不同的验证码 但是如如果接口相同则无法重复获取
    解决方法：加个data 随机传参

--- router/commonRouter/svgCaptcha.js
+++ router/commonRouter/svgCaptcha.js
@@ -4,5 +4,5 @@ const router = express.Router()
 const router_handle_common = require('../../router_handle/commonHandle/svgCaptcha')
 
 //获取图片验证码
-router.get('/getSvgCaptcha',router_handle_common.getSvgCaptcha)
+router.get('/getSvgCaptcha/:data',router_handle_common.getSvgCaptcha)
 module.exports=router
